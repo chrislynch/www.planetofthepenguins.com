7e05d4346bf7094068c343c0aad1a8bdee6eb64d
Chris Lynch <github@planetofthepenguins.com>
2013-02-02 21:28:01 +0000

    Stashing more changes

---
+++ 10.content/api/bubble/bubble.php
@@ -0,0 +1,89 @@
+<?php
+
+if (isset($_POST['bubbletext'])){
+    print Bubble($_POST['bubbletext']);
+}
+
+function Bubble($text){
+    $return = '';
+    $text = explode("\n",$text);
+    $currentline = '';
+    $currentpage = 1;
+    $currentpanel = 1;
+    
+    foreach($text as $textline){
+        $textlinechars = str_split($textline);
+        // Check for a new page or panel
+        if ($textlinechars[0] == '#'){
+            if ($textlinechars[1] !== '#'){
+                // New page
+                $currentline = 'page';
+                array_shift($textlinechars);
+                $textline = trim(implode($textlinechars));
+                $return .= "<p><font size='+1'><strong>PAGE $currentpage</strong></font></p>";
+                // Number the panels in the previous page
+                $prevpage = $currentpage -1;
+                if ($prevpage > 0){
+                    $panelcount = $currentpanel -1 ;
+                    $return = str_ireplace("<p><font size='+1'><strong>PAGE $prevpage</strong></font></p>", 
+                                            "<p><font size='+1'><strong>PAGE $prevpage ($panelcount PANELS)</strong></font></p>", $return);
+                }
+                $currentpage++;
+                $currentpanel = 1;
+            } else {
+                // New panel
+                $currentline = 'panel';
+                array_shift($textlinechars);
+                array_shift($textlinechars);
+                $textline = trim(implode($textlinechars));
+                $return .= "<p style='margin-top:8px;'><u>PANEL $currentpanel</u></p>";
+                $currentpanel++;
+            }
+        } else {
+            // Check for character or dialogue
+            if ($textlinechars[0] == ' ' OR $textlinechars[0] == "\t"){
+                if ($textlinechars[1] == ' ' OR $textlinechars[1] == "\t"){
+                    // Speech bubble
+                    $currentline = 'bubble';
+                    array_shift($textlinechars);
+                    array_shift($textlinechars);
+                    $textline = trim(implode($textlinechars));
+                    $textline = strtoupper($textline);
+                } else {
+                    // Character
+                    $currentline = 'character';
+                    array_shift($textlinechars);
+                    $textline = trim(implode($textlinechars));
+                    $textline = strtoupper($textline);
+                }
+            }
+        }
+        
+        if (trim($textline) == "\n" or trim($textline) == '') {
+            $textline = '<br>';
+        } else {
+            switch ($currentline){
+                case 'character':
+                    $return .= "<p align='center' style='margin-bottom:0'><strong>$textline</strong></p>";
+                    break;
+                case 'bubble':
+                    $return .= "<p align='center' style='margin-bottom:0'>$textline</p>";
+                    break;
+                case 'page':
+                case 'panel':
+                default:
+                    $return .= "<p>$textline</p>";
+                    break;
+            }
+        }
+    }
+
+    // Count panels in the final page
+    $prevpage = $currentpage -1;
+    $panelcount = $currentpanel -1 ;
+    $return = str_ireplace("<p><font size='+1'><strong>PAGE $prevpage</strong></font></p>", 
+                            "<br><p><font size='+1'><strong>PAGE $prevpage ($panelcount PANELS)</strong></font></p>", $return);
+    
+    return $return;
+}
+?>
