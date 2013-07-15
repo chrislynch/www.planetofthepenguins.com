ec7974ba79858eda9a748c5d0904412e90b82d2f
Chris Lynch <github@planetofthepenguins.com>
2013-07-16 00:12:35 +0100

    Link to Drupal DB

---
+++ 00.process/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+$this->_loadPlugin('db');
+$this->_config->set('mysql.server','mysql.planetofthepenguins.com');
+$this->_config->set('mysql.user','planetofthepengs');
+$this->_config->set('mysql.password','sp1der20');
+$this->_config->set('mysql.database','planetofthepenguinscom');
+?>
