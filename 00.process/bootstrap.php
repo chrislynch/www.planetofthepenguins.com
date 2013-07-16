a3b507bbb653917f1259ef9b2842bcdb4ce7ea7b
Chris Lynch <github@planetofthepenguins.com>
2013-07-16 23:46:04 +0100

    Removed file

--- 00.process/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->_loadPlugin('db');
-$this->_config->set('mysql.server','mysql.planetofthepenguins.com');
-$this->_config->set('mysql.user','planetofthepengs');
-$this->_config->set('mysql.password','sp1der20');
-$this->_config->set('mysql.database','planetofthepenguinscom');
-?>
