c85583840e395159ab2a781919e17cfa950d7d25
Chris Lynch <github@planetofthepenguins.com>
2013-07-19 09:50:27 +0100

    Sidebar to content bar

---
+++ 11.content_bar/_archive/old_sidebar.php
@@ -0,0 +1,41 @@
+<div id="sidebar" class="four columns omega">
+			<div id="sidebar-contents">
+				<!-- AddThis Button BEGIN -->
+				<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+				<a class="addthis_button_preferred_1"></a>
+				<a class="addthis_button_preferred_2"></a>
+				<a class="addthis_button_preferred_3"></a>
+				<a class="addthis_button_preferred_4"></a>
+				<a class="addthis_button_compact"></a>
+				<a class="addthis_counter addthis_bubble_style"></a>
+				</div>
+				<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50e999277381fa2b"></script>
+				<!-- AddThis Button END -->
+			    <?php print @$this->sidebar->html; ?>
+			</div>
+			<div id="sidebar-ads">
+				<div id="sidebar-ads-left" class="three columns alpha">
+					<!-- Amazon -->
+
+					<SCRIPT charset="utf-8" type="text/javascript" src="http://ws.amazon.co.uk/widgets/q?rt=tf_mfw&ServiceVersion=20070822&MarketPlace=GB&ID=V20070822/GB/chrislynch/8001/f64ff3fa-273d-4a29-bb8b-d84a1d67dc9d"> </SCRIPT> <NOSCRIPT><A HREF="http://ws.amazon.co.uk/widgets/q?rt=tf_mfw&ServiceVersion=20070822&MarketPlace=GB&ID=V20070822%2FGB%2Fchrislynch%2F8001%2Ff64ff3fa-273d-4a29-bb8b-d84a1d67dc9d&Operation=NoScript">Amazon.co.uk Widgets</A></NOSCRIPT>
+
+				</div>
+				<div id="sidebar-ads-right" class="three columns omega">
+					<!-- Google -->
+
+					<script type="text/javascript"><!--
+					google_ad_client = "ca-pub-1543873788475773";
+					/* Planet of the Penguins (e4) Narrower */
+					google_ad_slot = "8756055144";
+					google_ad_width = 120;
+					google_ad_height = 600;
+					//-->
+					</script>
+					<script type="text/javascript"
+					src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+					</script>
+				</div>
+
+			</div>
+			<script type="text/javascript" id="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=planetofthepenguins&height=2400&flexible=0&skip=about-site,show_rebelnav"></script>
+		    </div>
